739743e4725d64e2e532c1093a353538243aea79
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-09-12 23:58:51 +0100

    5/11

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log('working');
+
+// What are Functions??
+ // we can calculate the area of one rectangle with the following steps everything time!!
+
+const width = 10;
+const height = 6
+const area = width * height;
+console.log(area); //output will be 60
+
+//now imagine being asked to calculate the area of three different rectangles
+// so you would carry out the first method three times logically, however in programming language we can code to make a specific task run several times instead of writing it for each area of rectangle for example, we achieve this by writing a function.
+// FUNCTION is a reusable block of code that groups together a sequence of statements to perform a specific task.
+
+// what does a function declaratoin consist of??
+// function keyword followed by the identifier which is the name of the funtion by which you represent the function itself, and  the function body which is inside curly brackets {}.This is the block statemets required to perform a specific task, enclosed in the function.
+
+// we should be aware of the hoisting feature in javescript as it allows access to function declarations before they are defined.
+
+//Task done on codeacademy for creating a function that prints a reminder to the console. Using a function declaration, create a function called getReminder()
